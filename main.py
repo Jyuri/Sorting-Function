7a28fa998e6e7a6f8a426d581742d4e6859c48fe
Jyuri <jurgenmehlhaff@yahoo.com>
2021-11-11 16:04:35 -0800

    Function Established
    The main function was finally finished, other unnecessary functions were removed.

--- main.py
+++ main.py
@@ -2,25 +2,48 @@ from random import randrange
 from time import sleep
 import sys
 from tqdm import tqdm
+import PySimpleGUI as sg
 
-def disorganizedList(listLength):
-  unorgList = []
-  for i in range(listLength):
-    unorgList.append(i)
-  for ix in range(listLength):
-    iy = randrange(0,listLength)
-    x = unorgList[ix]
-    y = unorgList[iy]
-    unorgList[ix] = y
-    unorgList[iy] = x
-  return unorgList
+
+windowWidth=1400
+windowHeight=780
+numOfIntervals=500
+slope = windowHeight/windowWidth
+step = windowWidth/numOfIntervals
+
+myGraph = sg.Graph(
+  (windowWidth,windowHeight),
+  (0,0),
+  (windowWidth,windowHeight),
+  background_color='White', key='graph')
+
+window = sg.Window('Demo', layout=[[myGraph]], margins=(0,0),location=(0,0)).finalize()
+graph = window['graph']
+
+myLines = []
+for i in range(0, int(windowWidth/step)+1):
+  myLines.append(graph.draw_line(
+    (i*step,0),
+    (i*step,i*slope*step),
+    width=step)
+  )
+
+def shift(startVal, endVal):
+  graph.relocate_figure(myLines[startVal], endVal*step, 0)
+  graph.relocate_figure(myLines[endVal], startVal*step, 0)
+
+  tempS = myLines[startVal]
+  tempE = myLines[endVal]
+
+  myLines[startVal] = tempE
+  myLines[endVal] = tempS
+
+  window.finalize()
 
 def quickSort(x):
   i = 0
   numTrans = 0
   tempNumTrans = 0
-  # pbarCheck = tqdm(desc='Check Count', total = (len(x)**2))
-  pbarChange = tqdm(desc='Change Count')
   while True:
     if i == (len(x)-1):
       i = 0
@@ -29,20 +52,38 @@ def quickSort(x):
       elif tempNumTrans == numTrans:
         break
     if x[i] > x[i+1]:
+      shift(i, i+1)
+
       y = x[i]
       z = x[i+1]
       x[i] = z
       x[i+1]= y
       numTrans += 1
-      pbarChange.update(1)
+
+      #sleep(0.05)
     i += 1
-    # pbarCheck.update(1)
-  # pbarCheck.close()
-  pbarChange.close()
   return x
 
-
-
 if __name__ == "__main__":
-  disorgList = disorganizedList(10000)
-  orgList = quickSort(disorgList)
+  tempArray = []
+  for i in range(0, len(myLines)):
+    tempArray.append(i)
+
+  for iStart in range(0, len(myLines)):
+    iEnd = randrange(0, len(myLines))
+    shift(iStart, iEnd)
+    
+    x = tempArray[iStart]
+    y = tempArray[iEnd]
+    tempArray[iStart] = y
+    tempArray[iEnd] = x
+
+    #sleep(0.1)
+
+  quickSort(tempArray)
+  while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+      break
+
+window.close()
