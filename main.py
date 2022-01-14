d1d4e82328895ad008ef5af41715a3c0aebed08e
Jyuri <jurgenmehlhaff@yahoo.com>
2022-01-14 11:51:35 -0700

    Added Functionality
    New sorting functions included, including "ForwardAndBackSort" which compares values in increasing order as well as decreasing order. Also includes "chunkSorting" which splits values into groups of similar sized values. Currently, arrays can only be sorted into 4 groups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randrange
 from time import sleep
+
 import sys
 from tqdm import tqdm
 import PySimpleGUI as sg
@@ -28,7 +29,7 @@ for i in range(0, int(windowWidth/step)+1):
     width=step)
   )
 
-def shift(startVal, endVal):
+def graphicSwap(startVal, endVal):
   graph.relocate_figure(myLines[startVal], endVal*step, 0)
   graph.relocate_figure(myLines[endVal], startVal*step, 0)
 
@@ -40,6 +41,11 @@ def shift(startVal, endVal):
 
   window.finalize()
 
+def graphicShift(startVal, endVal):
+  graph.relocate_figure(myLines[startVal], endVal*step, 0)
+  window.finalize()
+
+#Function for traditional sorting method of moving the highest value right
 def quickSort(x):
   i = 0
   numTrans = 0
@@ -52,7 +58,7 @@ def quickSort(x):
       elif tempNumTrans == numTrans:
         break
     if x[i] > x[i+1]:
-      shift(i, i+1)
+      graphicSwap(i, i+1)
 
       y = x[i]
       z = x[i+1]
@@ -64,14 +70,116 @@ def quickSort(x):
     i += 1
   return x
 
+#Function for sorting by moving the greater value to the right, but compares values moving right and left.
+def ForwardAndBackSort(x):
+  i = 0
+  numTrans = 0
+  tempNumTrans = 0
+  forwardCondition = True
+  while True:
+    if i == (len(x)-1):
+      i = 0
+      if tempNumTrans != numTrans:
+        tempNumTrans = numTrans
+      elif tempNumTrans == numTrans:
+        break
+
+      if forwardCondition == True:
+        forwardCondition = False
+      elif forwardCondition == False:
+        forwardCondition = True
+
+    if forwardCondition == True:
+      if x[i] > x[i+1]:
+        graphicSwap(i, i+1)
+
+        y = x[i]
+        z = x[i+1]
+        x[i] = z
+        x[i+1]= y
+        numTrans += 1
+
+    elif forwardCondition == False:
+      iRev = (len(x) - 1 - i)
+      if x[iRev] < x[iRev-1]:
+        graphicSwap(iRev, iRev-1)
+
+        y = x[iRev]
+        z = x[iRev-1]
+        x[iRev] = z
+        x[iRev-1]= y
+        numTrans += 1
+
+    i += 1
+  return x
+
+#A semi-sorting function designed to split an array into 4 chunks of similar sizes.
+#Meant to work with another sorting function to speed up the sorting process of large arrays
+def chunkSorting(x):
+  chunkDef = len(x)/4
+  chunk1 = [[0,(chunkDef-1)],[]]
+  chunk2 = [[chunkDef,(chunkDef*2)],[]]
+  chunk3 = [[(chunkDef*2),(chunkDef*3)],[]]
+  chunk4 = [[(chunkDef*3),(chunkDef*4)],[]]
+  errChunk = []
+
+  for i in range(0,len(x)):
+    if chunk1[0][0] <= x[i] & x[i] <= chunk1[0][1]:
+      chunk1[1].append([x[i],i])
+    elif chunk2[0][0] <= x[i] & x[i] <= chunk2[0][1]:
+      chunk2[1].append([x[i],i])
+    elif chunk3[0][0] <= x[i] & x[i] <= chunk3[0][1]:
+      chunk3[1].append([x[i],i])
+    elif chunk4[0][0] <= x[i] & x[i] <= chunk4[0][1]:
+      chunk4[1].append([x[i],i])
+    else:
+      errChunk.append([x[i],i])
+  
+  global myLines
+  x = []
+  myNewLines = []
+  iT = 0
+  for i in range(0,len(chunk1[1])):
+    graphicShift(chunk1[1][i][1],iT)
+    iT += 1
+    myNewLines.append(myLines[chunk1[1][i][1]])
+    x.append(chunk1[1][i][0])
+  for i in range(0,len(chunk2[1])):
+    graphicShift(chunk2[1][i][1],iT)
+    iT += 1
+    myNewLines.append(myLines[chunk2[1][i][1]])
+    x.append(chunk2[1][i][0])
+  for i in range(0,len(chunk3[1])):
+    graphicShift(chunk3[1][i][1],iT)
+    iT += 1
+    myNewLines.append(myLines[chunk3[1][i][1]])
+    x.append(chunk3[1][i][0])
+  for i in range(0,len(chunk4[1])):
+    graphicShift(chunk4[1][i][1],iT)
+    iT += 1
+    myNewLines.append(myLines[chunk4[1][i][1]])
+    x.append(chunk4[1][i][0])
+  for i in range(0,len(errChunk)):
+    graphicShift(errChunk[i][1],iT)
+    iT += 1
+    myNewLines.append(myLines[errChunk[i][1]])
+    x.append(errChunk[i][0])
+  myLines = myNewLines
+
+  return x
+
+
 if __name__ == "__main__":
+  #Initialize array of increasing values
   tempArray = []
   for i in range(0, len(myLines)):
     tempArray.append(i)
 
+
+  #Random distribution of values from an organized list using randrange
   for iStart in range(0, len(myLines)):
     iEnd = randrange(0, len(myLines))
-    shift(iStart, iEnd)
+    graphicSwap(iStart, iEnd)
     
     x = tempArray[iStart]
     y = tempArray[iEnd]
@@ -80,7 +188,13 @@ if __name__ == "__main__":
 
     #sleep(0.1)
 
-  quickSort(tempArray)
+  #Traditional sorting method of moving the highest value right
+  #quickSort(tempArray)
+
+  #Similar to "Quick Sort" with the exception of scanning left, and then reversing right.
+  tempArray = chunkSorting(tempArray)
+  ForwardAndBackSort(tempArray)
+
   while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
